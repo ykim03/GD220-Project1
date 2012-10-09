acd384e5fbb682a899d24a2a47fc955fb0558b35
Yoojin Kim <ykim03@mica.edu>
2012-10-09 08:48:32 -0400

    catch up repo with changes made on computer
    Desktop and mobile design changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+$(function(){
+	
+	var sections = $('#content').find('.section')
+	
+		sections.hide().eq(0).show();
+	
+	var links = $('#header .nav a');
+		links.removeClass('here');
+		links.eq(0).addClass('here');
+	
+	links.on('click',function(e){
+		e.preventDefault();
+		var url = $(this).attr('href');
+		
+		$(this).addClass('here');
+		
+		sections.hide();
 
+		$(url).show();
+		
+	});
+	
+});
